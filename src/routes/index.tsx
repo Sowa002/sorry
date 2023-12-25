f3c36b2c962dbd0d6d0bdea917c3e2967dfc907c
Sowa002 <91929070+Sowa002@users.noreply.github.com>
2023-12-25 20:27:26 +0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,11 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>MAAFFF</Title>
+      <h1>nabila sayangggg</h1>
+      <h1>maaaf....</h1>
       <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+    
     </main>
   );
 }
